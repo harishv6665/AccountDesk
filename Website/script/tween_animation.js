540bcb89d8b11b8083b676f37ded683537f783b2
harish-rzt <harish.v@razorthink.net>
2016-09-03 15:50:14 +0530

    showing the header on scroll on both mobile and desktop

--- Website/script/tween_animation.js
+++ Website/script/tween_animation.js
@@ -1,9 +1,34 @@
 var MobileNavAnimation;
 var isMobileNavOpen = false;
 var mobileNavButton = document.querySelector('.ad-header-mobile-button');
+var pageHeader = document.querySelector('.ad-header');
+var windowHeight = window.outerHeight;
 
 mobileNavButton.addEventListener('click', handleMobileNavClick, false);
 document.addEventListener('click', closeMobileNav, false);
+document.addEventListener('scroll', handleDocumentScroll, false);
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
 
 function handleMobileNavClick(e) {
     e.stopPropagation();
@@ -37,5 +62,18 @@ function closeMobileNav() {
     if(isMobileNavOpen) {
         isMobileNavOpen = false;
         MobileNavAnimation.timeScale(1.2).reverse();
+    }
+}
+
+function handleDocumentScroll() {
+    if(window.pageYOffset > 300) {
+        addClass(pageHeader, "fixed-header");
+        if(window.pageYOffset > windowHeight) {
+            addClass(pageHeader, "slide-in");
+        } else {
+            removeClass(pageHeader, "slide-in");
+        }
+    } else {
+        removeClass(pageHeader, "fixed-header");
     }
 }
